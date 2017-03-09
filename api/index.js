48692e422418e63d7de8cc632d7a590bec3f1ed1
JOE TIMKO <jmt53@duke.edu>
2017-03-09 18:42:29 -0500

    index

---
+++ api/index.js
@@ -0,0 +1,25 @@
+var http = require('http'),
+    express = require('express'),
+    MongoClient = require('mongodb').MongoClient,
+    assert = require('assert'),
+    bodyParser = require('body-parser');
+
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+
+var mongoHost = 'localHost'; //A
+var mongoPort = 27017;
+
+app.use(bodyParser.json())
+
+
+var url = 'mongodb://localhost:27017/openwater';
+var mdb = null;
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+  mdb = db
+
+});
