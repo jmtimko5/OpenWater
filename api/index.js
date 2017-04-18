014a7ab4d67441c4b5eff80b83fc848d2b653b4c
JOE TIMKO <jmt53@duke.edu>
2017-04-18 14:40:02 -0400

    created react-native project

--- api/index.js
+++ api/index.js
@@ -23,3 +23,9 @@ MongoClient.connect(url, function(err, db) {
   mdb = db
 
 });
+
+app.get('/')
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
